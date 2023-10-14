c56b8d1a6d4fea321ba12ab06041777b21e6f285
Anton <anton.langstein@gmail.com>
2023-10-14 23:58:05 +0200

    restart

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -9,69 +9,25 @@ import { Container } from "react-bootstrap";
 const socket = io.connect("http://localhost:3001");
 const rndNum = Math.floor(Math.random() * 100).toString();
 
-type User = {
-  id: string;
-  name: string;
-  color: { bg: string; font: string };
-};
+// type User = {
+//   id: string;
+//   name: string;
+//   color: { bg: string; font: string };
+//   isAdmin: boolean;
+//   playedCard: string;
+// };
 
 function App() {
-  const [users, setUsers] = useState<User[]>();
-  const [user, setUser] = useState<User>();
-  const [id, setId] = useState<string>("");
-  const [name, setName] = useState<string>("Anton" + rndNum);
-  const [color, setColor] = useState({});
-
-  const setOwnUser = (_users) => {
-    _users.forEach((user) => {
-      if (user.id === socket.id) {
-        setUser(user);
-        setColor(user.color);
-      }
-    });
-  };
-
   useEffect(() => {
-    socket.on("set_users", (item: User[]) => {
-      setUsers(item);
-      setOwnUser(item);
-    });
-
-    socket.on("new_user", () => {
-      setId(socket.id);
-      socket.emit("send_name_to_server", { name, id: socket.id });
-    });
-
     return () => {
-      socket.off("new_user");
-      socket.off("set_users");
+      socket.off("");
     };
   }, [socket]);
 
   return (
     <>
       <main>
-        <Container>
-          {!name && <Login setName={setName} />}
-          {name && (
-            <div className="app">
-              <Game
-                name={name}
-                user={user}
-                users={users}
-                setUsers={setUsers}
-                socket={socket}
-                identifier={id}
-              ></Game>
-              <Chat
-                socket={socket}
-                identifier={id}
-                name={name}
-                color={user?.color}
-              ></Chat>
-            </div>
-          )}
-        </Container>
+        <Container>Ich bin ein text</Container>
       </main>
     </>
   );
